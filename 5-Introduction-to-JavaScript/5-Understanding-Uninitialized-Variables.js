a7cfc1cbef242b7dd079ece5ac4d6974ffcf9fde
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-03 07:15:25 -0600

    Initialize three variables a, b, and c with 5, 10, and "I am a" respectively

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,11 @@
+<!--I learned when JavaScript variables are initially declared they have a value of undefined and if used in a math operation the result will be NaN-->
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a"
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
