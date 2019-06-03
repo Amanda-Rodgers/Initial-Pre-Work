8bf08b4802b59f56b6018ca563e102bf8f4cc2e3
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-03 11:29:55 -0600

    Modify existing declarations and assignments so their names use camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+<!-- I learned the proper way and reason to use camelCase-->
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
