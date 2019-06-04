8a830e08c39f7e5be543862e2935f1ba2a249d86
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-04 09:46:39 -0600

    Set myName to a string equal to your name and build myStr with myName between the strings "My name is " and " and I am well!"

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+//I learned how to construct a string with variables
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Amanda Rodgers"
+var myStr = "My name is " + myName + "and I am well!";
