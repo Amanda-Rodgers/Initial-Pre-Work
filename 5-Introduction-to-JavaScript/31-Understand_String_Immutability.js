a4addec5faa756ae4516b8863dc703f01f062b1b
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-04 11:28:02 -0600

    Correct the assignment to myStr so it contains the string value of Hello World

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// I learned that you can not change the value of a variable by changing individual characters
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
