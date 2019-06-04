15e73011d3e5d3bee0af597fb5a76f67400e26b5
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-03 18:04:33 -0600

    Create two new string variables: myFirstName and myLastName and assign them values of your first and last name, respectively

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,7 @@
+// I learned how to declare a sting variables
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+var myFirstName = "Amanda";
+var myLastName = "Rodgers";
+// Only change code below this line
