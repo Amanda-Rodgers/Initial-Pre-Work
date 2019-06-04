2a5448426dbd036ab37dfbea9b35d3be92b8af3f
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-04 09:34:27 -0600

    Build myStr over several lines by concatenating these two strings: "This is the first sentence. " and "This is the second sentence." using the += operator

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+//I learned how to concatenate strings with the Plus Equals Operator
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
