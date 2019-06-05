d9651aa291d1dc84fcbf9c82d9c8807837afb493
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-05 14:23:02 -0600

    Call processArg function with an argument of 7 and assign its return value to the variable processed

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,18 @@
+//I learned that you can take the return value from a function and assign it to a variables// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
