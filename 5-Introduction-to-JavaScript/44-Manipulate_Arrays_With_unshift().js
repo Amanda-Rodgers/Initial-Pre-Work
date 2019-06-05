bee19797b7af62a2bbd4743b7703d8c850dae31d
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-05 10:02:34 -0600

    Add ["Paul",35] to beginning of myArray variable using unshift()

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,12 @@
+// I learned how to add an item to the beginning of an array by using .unshift ()
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul",35]);
+// Only change code below this line.
