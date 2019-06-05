8a177294cbd5035bf839abab00228c81ce1b6ce9
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-05 09:06:25 -0600

    Modify new array myArray so that it contains both a string and a number in that order

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+// I learned that you can store multiple values in one variable using an array
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["string", 4];
