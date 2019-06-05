b6d016dba315a7cdc23c9922b54f8c9c17e74fe4
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-05 09:25:13 -0600

    Create a variable called myData and set it to equal the first value of myArray using bracket notation

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+//I learned how to access array data with indexes
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+var myData = myArray[0];
+
+// Only change code below this line.
