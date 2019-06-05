f2cfb494734db98b18acff43b3fca85bd8bf7fcb
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-05 14:11:04 -0600

    Create a function addFive without any arguments This function adds 5 to the sum variable, but its returned value is undefined

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,15 @@
+// I learned that when a function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive () {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
