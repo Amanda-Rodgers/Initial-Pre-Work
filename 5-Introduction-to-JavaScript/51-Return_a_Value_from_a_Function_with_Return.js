7e5f1860fbb063dbc8c3dc694c127d3b229b71ef
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-05 12:53:23 -0600

    Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+//I learned how to return a value from a math operation using a function
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
