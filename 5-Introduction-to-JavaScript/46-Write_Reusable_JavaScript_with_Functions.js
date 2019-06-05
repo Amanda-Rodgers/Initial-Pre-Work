1da68ee6eb3c71a33abf5660de8500d01fb43e91
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-05 10:19:52 -0600

    Create a function called reusableFunction that prints "Hi World" to the dev console
    Call the function

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+// I learned how to write a reusable function in JavaScript
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
