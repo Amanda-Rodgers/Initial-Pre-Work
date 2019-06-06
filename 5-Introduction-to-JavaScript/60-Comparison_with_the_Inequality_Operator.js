52e41930f46b40abde4bf838b0e1a6f2d9623261
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-06 12:35:59 -0600

    Add inequality operator != in the if statement so that the function will return "Not Equal" when val is not equivalent to 99

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,10 @@
+// I learned how to use the inequality Operator// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(11);
