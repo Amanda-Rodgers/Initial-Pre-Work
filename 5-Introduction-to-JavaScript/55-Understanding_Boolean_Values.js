780fe90df73dab5c24c6621ccc0a62d9b5aedc5a
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-06 09:01:36 -0600

    Modify welcomeToBooleans function so that it returns true instead of false

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+//I learned that a Boolean is a data type that can only be true or false
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
