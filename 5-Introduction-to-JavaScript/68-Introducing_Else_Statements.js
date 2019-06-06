3bfb9bc9703600152be583c9d46706e2dc974074
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-06 17:05:26 -0600

    Combine the if statements into a single if/else statement

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,17 @@
+// I learned how to write an if/else function
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    return "Bigger than 5";
+  } else {
+    return "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
