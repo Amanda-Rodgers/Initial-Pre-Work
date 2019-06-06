b96b8673b0d491ffcb1dfa55cb216d274994e059
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-06 12:47:26 -0600

    Add strict inequality operator to the if statement so the function will return "Not Equal" when val is not strictly equal to 17

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// I learned how to use the strict inequality operator
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
