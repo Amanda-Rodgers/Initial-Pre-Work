f4dea99d7209795d9c1c1e0069d5e0b8b1842c96
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-06 16:29:14 -0600

    Combine the two if statements into one statement which returns "Outside" if val is not between 10 and 20, inclusive, otherwise, return "Inside"

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,17 @@
+// I learned how to use the logical or Operator
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+
+
+
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
