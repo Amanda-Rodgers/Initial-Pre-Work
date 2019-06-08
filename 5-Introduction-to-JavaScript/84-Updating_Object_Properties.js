b76d73d6df079053dfaf16eebfe948b3846bfca7
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-07 18:12:00 -0600

    Update myDog object's name property to "Happy Coder"

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,22 @@
+// I learned how to update an object's property
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.name = "Happy Coder";
+
+// Only change code below this line.
