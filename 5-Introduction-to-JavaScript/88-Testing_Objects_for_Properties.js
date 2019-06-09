136b7cb1b63366a8d0381210a0061a5fb6a9b472
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-09 07:10:04 -0600

    Modify function checkObj to test myObj for checkProp, if property is found, return that property's value. If not, return "Not Found"

---
+++ 5-Introduction-to-JavaScript/88-Testing_Objects_for_Properties.js
@@ -0,0 +1,19 @@
+// I learned how to use the .hasOwnProperty(propname) method to check if the property of a given object exists or not
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+if (myObj.hasOwnProperty(checkProp)) {
+  return myObj[checkProp];
+} else {
+  return "Not Found";
+}
+}
+
+// Test your code by modifying these values
+checkObj("gift");
