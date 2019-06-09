08c205525afa74a45491e877f440195fac7b15dc
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-09 16:37:03 -0600

    Change randomFraction to return a random number instead of returning 0

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+//I learned how to generate random fractions with the Math.random() function
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
