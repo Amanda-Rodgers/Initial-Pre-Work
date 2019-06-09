30927fc9344331af42edec36bedb3bc52ac11699
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-09 10:29:42 -0600

    Push the numbers 0 through 4 to myArray using a while loop

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,10 @@
+// I learned how to use a while loop to iterate values through a function
+// Setup
+var myArray = [];
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++
+}
+
+// Only change code below this line.
