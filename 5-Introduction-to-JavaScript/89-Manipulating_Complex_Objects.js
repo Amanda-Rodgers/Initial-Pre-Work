1831be4701348010c56453c5d6ca49706614f274
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-09 07:56:12 -0600

    Add new album to myMusic array that includes: artist, title strings, release_year number, and a formats array of strings

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,24 @@
+// I learned how to create a complexe object
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+  {
+    "artist": "Princess",
+    "title": "OutThere",
+    "release_year" : 2019,
+    "formats" : [
+      "Live",
+      "mp4",
+    ]
+  }
+];
