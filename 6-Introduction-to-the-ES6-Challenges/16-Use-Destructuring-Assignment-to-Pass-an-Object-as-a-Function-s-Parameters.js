a6fe10223065e14b30db3ff90a3cdab655ee2fcb
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-10 14:57:00 -0600

    Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a

---
+++ 6-Introduction-to-the-ES6-Challenges/16-Use-Destructuring-Assignment-to-Pass-an-Object-as-a-Function-s-Parameters.js
@@ -0,0 +1,10 @@
+// I learned how to destructure an assignment to pass an object as a function's parameter
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [a,b] = [b,a];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
