6ee621006510d4d5481d813bd1a87d3a1777e7ee
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-11 16:57:33 -0600

    Correct comment out syntax to JavaScript

--- 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -1,4 +1,4 @@
-<!--I learned when JavaScript variables are initially declared they have a value of undefined and if used in a math operation the result will be NaN-->
+//<!--I learned when JavaScript variables are initially declared they have a value of undefined and if used in a math operation the result will be NaN-->
 // Initialize these three variables
 var a = 5;
 var b = 10;
