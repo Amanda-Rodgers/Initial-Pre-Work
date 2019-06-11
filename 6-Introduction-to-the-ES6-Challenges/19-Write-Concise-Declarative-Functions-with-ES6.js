6e87c45fe638bf0baa3d3c50cc71d86923ba519b
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-11 05:34:36 -0600

    Refactor the function setGear inside the object bicycle to use the shorthand syntax described above

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,11 @@
+// I learned how to concise declarative functions with ES6
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    return this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
