93b394ea9831e4db2087c4f21f3ca8feefc010e6
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-11 06:04:29 -0600

    Use class keyword and write a proper constructor to create the Vegetable class with a property name

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,11 @@
+// I learned how to use class syntax to define a constructor function
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
