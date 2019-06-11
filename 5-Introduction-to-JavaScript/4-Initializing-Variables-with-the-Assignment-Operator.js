752a2a2cdb077fdecf2464ba3d1f8041fc560283
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-11 16:57:09 -0600

    Correct comment out syntax to JavaScript

--- 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -1,3 +1,3 @@
-<!--I learned that it is common to initialize a variable to an initial value in the same line as it is declared-->
+//<!--I learned that it is common to initialize a variable to an initial value in the same line as it is declared-->
 var ourVar = 19;
 var a= 9;
