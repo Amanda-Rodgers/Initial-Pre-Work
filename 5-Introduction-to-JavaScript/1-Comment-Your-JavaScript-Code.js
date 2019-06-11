31039a93c04da72d9de1eba96d221f812877dc6a
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-11 16:55:02 -0600

    Correct comment out syntax to JavaScript

--- 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
+++ 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
@@ -1,5 +1,5 @@
-<!--I learned how to comment out lines and multi-lines-->
-
+//<!--I learned how to comment out lines and multi-lines-->
+// CORRECTED CODE: This is the correct way to comment out in JavaScript and paying attention to different languages syntax!
 // This is just a comment
 /* Use this to make multi-line comments
 like this, stars are in */
