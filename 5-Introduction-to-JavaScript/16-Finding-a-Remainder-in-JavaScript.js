9b0aa96a61a40511b4453e7d84e726635c9e56a1
Amanda  Rodgers <aarodgerswork@gmail.com>
2019-06-12 14:22:02 -0600

    Correct comment out syntax to JavaScript

--- 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -1,2 +1,2 @@
-<!-- I learned that the remainder operator % gives the remainder of the division of two numbers-->
+//<!-- I learned that the remainder operator % gives the remainder of the division of two numbers-->
 var remainder = 11 % 3;
